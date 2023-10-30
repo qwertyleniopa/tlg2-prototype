278a0d886b7c4f361a5951a85ffcd2e4ea5b0415
qwertyleniopa <qwertyleniopa@gmail.com>
2023-10-30 10:12:20 +0800

    Allow join links from static hosting services

--- src/views/ConnectionView.svelte
+++ src/views/ConnectionView.svelte
@@ -1,13 +1,40 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
+    import { createEventDispatcher } from "svelte";
     const dispatch = createEventDispatcher();
 
     export let selfId;
     let peerId;
 
-    $: selfIdDisplay = selfId || '[loading...]';
-    $: inputDisabled = !peerId || peerId.trim() === '';
+    $: selfIdDisplay = selfId || "[loading...]";
+    $: inputDisabled = !peerId || peerId.trim() === "";
 </script>
+
+<div class="connection_view">
+    <div class="section">
+        <h1>The Less Genius Game</h1>
+        <small>(prototype)</small>
+    </div>
+
+    <!-- 
+        TODO: Writing to clipboard requires an HTTPS connection which I'm too
+        lazy to work on right now. We settle with just displaying the join link. 
+    -->
+    <div class="section">
+        <p>Your ID is: <em>{selfIdDisplay}</em>.</p>
+        <p>Join link: <em>{location.origin}?id={selfId}</em>.</p>
+        <p>Connect with another player or wait to be connected to.</p>
+    </div>
+
+    <div class="section">
+        <input type="text" placeholder="Opponent ID" bind:value={peerId} />
+        <button
+            disabled={inputDisabled}
+            on:click={() => dispatch("connect", peerId)}
+        >
+            Play!
+        </button>
+    </div>
+</div>
 
 <style>
     .connection_view {
@@ -27,30 +54,3 @@
         gap: 0.25rem;
     }
 </style>
-
-<div class="connection_view">
-    <div class="section">
-        <h1>The Less Genius Game</h1>
-        <small>(prototype)</small>
-    </div>
-
-    <!-- 
-        TODO: Writing to clipboard requires an HTTPS connection which I'm too
-        lazy to work on right now. We settle with just displaying the join link. 
-    -->
-    <div class="section">
-        <p>Your ID is: <em>{selfIdDisplay}</em>.</p>
-        <p>Join link: <em>{location.href}connect?id={selfId}</em>.</p>
-        <p>Connect with another player or wait to be connected to.</p>
-    </div>
-
-    <div class="section">
-        <input type="text" placeholder="Opponent ID" bind:value={peerId}>
-        <button 
-            disabled={inputDisabled} 
-            on:click={() => dispatch('connect', peerId)}
-        >
-            Play!
-        </button>
-    </div>
-</div>
